958626022933145b8e4bb3f43947188f84012dcd
Craig <indigomx9@gmail.com>
2022-03-17 15:50:28 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,8 @@
+# React 18
+
+Initial setup of the project.  Created with React 18, Webpack, and TypeScript.
+
+
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Andre from "../img/Andre.webp";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus facilis veniam necessitatibus sequi assumenda? Laborum aperiam voluptate illum perferendis, et dolorum itaque, nesciunt, consectetur exercitationem eveniet atque? Pariatur, animi laboriosam?</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
